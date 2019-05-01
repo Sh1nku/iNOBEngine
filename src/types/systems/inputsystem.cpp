c726264c8d3c24dccc5bb9ec5e73e38589712ceb
Sh1nku <hjandreas@hotmail.no>
2019-05-01 19:09:34 +0200

    Fixing error where keydown and up events were not reset

--- src/types/systems/inputsystem.cpp
+++ src/types/systems/inputsystem.cpp
@@ -11,35 +11,43 @@ InputSystem::~InputSystem() {
 }
 
 void InputSystem::Update(float dt) {
+	for (auto& keys : keyCodes) {
+		std::get<0>(keys.second) = false;
+		std::get<1>(keys.second) = false;
+	}
 	while (SDL_PollEvent(&e)) {
 		switch (e.type) {
 			case SDL_KEYDOWN:
 			{
-				auto keyDown = e.key.keysym.sym;
-				try {
-					auto& tupleDown = keyCodes.at(keyDown);
-					std::get<0>(tupleDown) = true;
-					std::get<1>(tupleDown) = false;
-					std::get<2>(tupleDown) = true;
+				if (e.key.repeat == 0) {
+					auto keyDown = e.key.keysym.sym;
+					try {
+						auto& tupleDown = keyCodes.at(keyDown);
+						std::get<0>(tupleDown) = true;
+						std::get<1>(tupleDown) = false;
+						std::get<2>(tupleDown) = true;
+					}
+					catch (std::out_of_range) {
+						keyCodes.emplace(keyDown, std::make_tuple<bool, bool, bool>(true, false, true));
+					}
+					break;
 				}
-				catch (std::out_of_range) {
-					keyCodes.emplace(keyDown, std::make_tuple<bool, bool, bool>(true, false, true));
-				}
-				break;
 			}
 			case SDL_KEYUP:
 			{
-				auto keyUp = e.key.keysym.sym;
-				try {
-					auto& tupleUp = keyCodes.at(keyUp);
-					std::get<0>(tupleUp) = false;
-					std::get<1>(tupleUp) = true;
-					std::get<2>(tupleUp) = false;
+				if (e.key.repeat == 0) {
+					auto keyUp = e.key.keysym.sym;
+					try {
+						auto& tupleUp = keyCodes.at(keyUp);
+						std::get<0>(tupleUp) = false;
+						std::get<1>(tupleUp) = true;
+						std::get<2>(tupleUp) = false;
+					}
+					catch (std::out_of_range) {
+						keyCodes.emplace(keyUp, std::make_tuple<bool, bool, bool>(false, true, false));
+					}
+					break;
 				}
-				catch (std::out_of_range) {
-					keyCodes.emplace(keyUp, std::make_tuple<bool, bool, bool>(false, true, false));
-				}
-				break;
 			}
 		}
 
