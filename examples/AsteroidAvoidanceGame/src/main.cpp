62b96a3b7020aa5ede7ea501515444f3bcc76c1e
Sh1nku <42642351+Sh1nku@users.noreply.github.com>
2021-07-07 22:51:16 +0200

    Update comments

--- examples/AsteroidAvoidanceGame/src/main.cpp
+++ examples/AsteroidAvoidanceGame/src/main.cpp
@@ -5,8 +5,8 @@ int main(int argc, char **argv) {
 	// Init GUI, HOLY FUCKING SHIT; THIS NEEDS TO BE AS EARLY IN THE APPLICATION AS POSSIBLE, OR ELSE IT WILL SPAWN MULTIPLE WINDOWS USING THE ACTUAL MAIN FUNCTION, WHAT A FUCKING USELESS FEATURE
 	//https://www.magpcss.org/ceforum/viewtopic.php?f=6&t=11977
 
-	//And of course for some reason they have literally changed the constructor depending on platform, so on windows you cannot pass the arguments that is actually needed for it to coordinate multiple processes
-	//Somehow it actually still works though. If it ever stops working, enable settings.single_process before CefInitialize
+	//And of course for some reason they have literally changed the constructor depending on platform, so on windows you cannot pass the arguments that are actually needed for it to coordinate multiple processes
+	//Somehow it actually still works though, it manages to get the arguments to the child processes from somewhere. If it ever stops working, enable settings.single_process before CefInitialize
 #ifdef _WIN32
 	CefMainArgs args;
 #else
